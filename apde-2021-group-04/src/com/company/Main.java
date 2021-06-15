cd072405523659682c804443f6b175a0b6a52abc
Ab Boukari <71227631+Abboukari@users.noreply.github.com>
2021-06-15 08:08:54 +0200

    Upload project

---
+++ apde-2021-group-04/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hardcode objects = new Hardcode();
+        objects.makeMeTheObjects();
+
+        Menu makeMenu = new Menu();
+        makeMenu.startMenu();
+    }
+}
